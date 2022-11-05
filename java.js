0f24c82a7c4ea7b989878577cf574109deee8c35
othmane <othmane>
2022-11-05 22:18:47 +0100

    ex5

--- java.js
+++ java.js
@@ -1,22 +1,11 @@
-let x = prompt();
 
-let y = prompt();
 
-let z = prompt();
-
-let w = prompt();
-
-let b = prompt();
-
-let tab = [x,y,z,w,b];
-
-let max = tab[0];
-
-for(let i = 0; i < 5 ; i++)
+tab=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+for(let i = 0; i < 16 ; i++)
 {
-    if(Number(tab[i]) > max){
-        max = tab[i];
+    if(Number(tab[i])%2==0){
+       alert(tab[i]); alert("nombre pair");
     }
+    else{alert(tab[i]); alert("nombre impair")} 
 }
 
-alert(max);
