651d326ac0bc8173d9e073ce4a508a833a979c82
othmane <othmane>
2022-11-05 22:32:51 +0100

    ex6

--- java.js
+++ java.js
@@ -3,9 +3,10 @@
 tab=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 for(let i = 0; i < 16 ; i++)
 {
-    if(Number(tab[i])%2==0){
-       alert(tab[i]); alert("nombre pair");
+    if(Number(tab[i])%3==0 && Number(tab[i])%5!=0){
+       alert("multiple de trois");
     }
-    else{alert(tab[i]); alert("nombre impair")} 
+    else if(Number(tab[i])%5==0 &&  Number(tab[i])%3!=0){ alert("multiple de cinq")}
+     else if(Number(tab[i])%5!=0 && Number(tab[i])%3!=0){ alert(tab[i]);}
 }
 
